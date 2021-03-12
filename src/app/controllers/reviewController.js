9c3eebeb78a4c099cac7ab0030582218d1657e75
강희윤 <khykhy1006@gmail.com>
2021-03-13 02:23:02 +0900

    pull

--- src/app/controllers/reviewController.js
+++ src/app/controllers/reviewController.js
@@ -11,8 +11,15 @@ const { constants } = require("buffer");
 
 exports.addReview = async function (req, res) {
   const token = req.verifiedToken;
-  const { reviewContent, vaccineName } = req.body;
+  const { reviewName, reviewContent, vaccineName } = req.body;
 
+  if (!reviewName) {
+    return res.json({
+      isSuccess: false,
+      code: 2001,
+      message: "후기 제목을 입력해주세요",
+    });
+  }
   if (!reviewContent) {
     return res.json({
       isSuccess: false,
@@ -40,6 +47,7 @@ exports.addReview = async function (req, res) {
 
     const addReviewResult = await reviewDao.insertReview(
       token.userIdx,
+      reviewName,
       reviewContent,
       vaccineName
     );
@@ -62,20 +70,16 @@ exports.addReview = async function (req, res) {
 
 exports.getReview = async function (req, res) {
   const token = req.verifiedToken;
+  var { standard, vaccineName1, vaccineName2, vaccineName3 } = req.query;
+  //stardard 1: 실시간순, 2: 좋아요순
 
-  if (!reviewContent) {
-    return res.json({
-      isSuccess: false,
-      code: 2002,
-      message: "후기 내용을 입력해주세요",
-    });
-  }
-  if (!vaccineName) {
-    return res.json({
-      isSuccess: false,
-      code: 2003,
-      message: "사용한 백신을 알려주세요",
-    });
+  if (vaccineName1) {
+    if (vaccineName2) {
+      if (!vaccineName3) vaccineName3 = "";
+    } else {
+      vaccineName2 = "";
+      vaccineName3 = "";
+    }
   }
 
   try {
@@ -87,25 +91,56 @@ exports.getReview = async function (req, res) {
         code: 2100,
         message: "탈퇴하거나 비활성화된 유저입니다",
       });
+    if (standard) {
+      if (standard != 1 && standard != 2)
+        return res.json({
+          isSuccess: false,
+          code: 2002,
+          message:
+            "정렬기준을 null, 1(실시간순), 2(좋아요순) 중에서 하나로 주세요. null일 경우 1로 간주됩니다",
+        });
+    }
 
-    const addReviewResult = await reviewDao.insertReview(
-      token.userIdx,
-      reviewContent,
-      vaccineName
-    );
-    if (addReviewResult.isSuccess == false) return addReviewResult;
-    return res.json({
-      isSuccess: true,
-      code: 1000,
-      userIdx: token.userIdx,
-      message: "후기 작성 성공",
-    });
+    if (standard == 2) {
+      const selectReviewLikeResult = await reviewDao.selectReviewLike(
+        token.userIdx,
+        vaccineName1,
+        vaccineName2,
+        vaccineName3
+      );
+      if (selectReviewLikeResult.isSuccess == false)
+        return selectReviewLikeResult;
+      return res.json({
+        isSuccess: true,
+        code: 1000,
+        userIdx: token.userIdx,
+        message: "좋아요순 후기 조회 성공",
+        result: selectReviewLikeResult,
+      });
+    } else {
+      const selectReviewCreateResult = await reviewDao.selectReviewCreate(
+        token.userIdx,
+        vaccineName1,
+        vaccineName2,
+        vaccineName3
+      );
+      console.log(token.userIdx, vaccineName1, vaccineName2, vaccineName3);
+      if (selectReviewCreateResult.isSuccess == false)
+        return selectReviewCreateResult;
+      return res.json({
+        isSuccess: true,
+        code: 1000,
+        userIdx: token.userIdx,
+        message: "실시간순 리뷰 조회 성공",
+        result: selectReviewCreateResult,
+      });
+    }
   } catch (error) {
     return res.json({
       isSuccess: false,
       code: 2000,
       userIdx: token.userIdx,
-      message: "후기 작성 실패",
+      message: "후기 조회 실패",
     });
   }
 };
--- src/app/dao/reviewDao.js
+++ src/app/dao/reviewDao.js
@@ -47,7 +47,190 @@ async function insertReview(userIdx, eviewContent, vaccineName) {
     });
   }
 }
+
+// 후기 조회(생성일순)
+async function selectReviewCreate(
+  userIdx,
+  vaccineName1,
+  vaccineName2,
+  vaccineName3
+) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  try {
+    const selectReviewQuery = `
+    select Review.reviewIdx, Review.userIdx,
+       User.userNickname, User.userProfileImgLink, reviewContent, vaccineName,
+       (select count(*) from LikeReview where LikeReview.status = 1 and LikeReview.reviewIdx = Review.reviewIdx) as likeReviewCount,
+       (select CASE WHEN LikeReview.status = 1 THEN 1 ELSE 0 END) as likeStatus,
+       case
+           when TIMESTAMPDIFF(MINUTE,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(SECOND,Review.createdAt,now()), '초전')
+           when TIMESTAMPDIFF(HOUR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MINUTE,Review.createdAt,now()), '분전')
+           when TIMESTAMPDIFF(DAY,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(HOUR,Review.createdAt,now()), '시간전')
+           when TIMESTAMPDIFF(WEEK,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(DAY,Review.createdAt,now()), '일전')
+           when TIMESTAMPDIFF(MONTH,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(WEEK,Review.createdAt,now()), '주전')
+           when TIMESTAMPDIFF(YEAR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MONTH,Review.createdAt,now()), '달전')
+           else concat(TIMESTAMPDIFF(YEAR, Review.createdAt, now()), '년전') end as createdAt
+    from Review
+            left join User on Review.userIdx = User.userIdx
+            left join LikeReview on LikeReview.userIdx = ? and Review.reviewIdx = LikeReview.reviewIdx
+    where if(? is not null,  vaccineName = ?, 1=1) and Review.status = 1
+    union
+    select Review.reviewIdx, Review.userIdx,
+          User.userNickname, User.userProfileImgLink, reviewContent, vaccineName,
+          (select count(*) from LikeReview where LikeReview.status = 1 and LikeReview.reviewIdx = Review.reviewIdx) as likeReviewCount,
+          (select CASE WHEN LikeReview.status = 1 THEN 1 ELSE 0 END) as likeStatus,
+          case
+              when TIMESTAMPDIFF(MINUTE,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(SECOND,Review.createdAt,now()), '초전')
+              when TIMESTAMPDIFF(HOUR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MINUTE,Review.createdAt,now()), '분전')
+              when TIMESTAMPDIFF(DAY,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(HOUR,Review.createdAt,now()), '시간전')
+              when TIMESTAMPDIFF(WEEK,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(DAY,Review.createdAt,now()), '일전')
+              when TIMESTAMPDIFF(MONTH,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(WEEK,Review.createdAt,now()), '주전')
+              when TIMESTAMPDIFF(YEAR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MONTH,Review.createdAt,now()), '달전')
+              else concat(TIMESTAMPDIFF(YEAR, Review.createdAt, now()), '년전') end as createdAt
+    from Review
+            left join User on Review.userIdx = User.userIdx
+            left join LikeReview on LikeReview.userIdx = ? and Review.reviewIdx = LikeReview.reviewIdx
+    where if(? is not null,  vaccineName = ?, 1=1) and Review.status = 1
+    union
+    select Review.reviewIdx, Review.userIdx,
+          User.userNickname, User.userProfileImgLink, reviewContent, vaccineName,
+          (select count(*) from LikeReview where LikeReview.status = 1 and LikeReview.reviewIdx = Review.reviewIdx) as likeReviewCount,
+          (select CASE WHEN LikeReview.status = 1 THEN 1 ELSE 0 END) as likeStatus,
+          case
+              when TIMESTAMPDIFF(MINUTE,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(SECOND,Review.createdAt,now()), '초전')
+              when TIMESTAMPDIFF(HOUR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MINUTE,Review.createdAt,now()), '분전')
+              when TIMESTAMPDIFF(DAY,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(HOUR,Review.createdAt,now()), '시간전')
+              when TIMESTAMPDIFF(WEEK,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(DAY,Review.createdAt,now()), '일전')
+              when TIMESTAMPDIFF(MONTH,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(WEEK,Review.createdAt,now()), '주전')
+              when TIMESTAMPDIFF(YEAR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MONTH,Review.createdAt,now()), '달전')
+              else concat(TIMESTAMPDIFF(YEAR, Review.createdAt, now()), '년전') end as createdAt
+    from Review
+            left join User on Review.userIdx = User.userIdx
+            left join LikeReview on LikeReview.userIdx = ? and Review.reviewIdx = LikeReview.reviewIdx
+    where if(? is not null,  vaccineName = ?, 1=1) and Review.status = 1
+    group by Review.reviewIdx, Review.createdAt
+    order by createdAt desc;
+                  `;
+    const selectReviewParams = [
+      userIdx,
+      vaccineName1,
+      vaccineName1,
+      userIdx,
+      vaccineName2,
+      vaccineName2,
+      userIdx,
+      vaccineName3,
+      vaccineName3,
+    ];
+    const [selectReviewRows] = await connection.query(
+      selectReviewQuery,
+      selectReviewParams
+    );
+    connection.release();
+    return selectReviewRows;
+  } catch (err) {
+    connection.release();
+    return res.json({
+      isSuccess: false,
+      code: 4000,
+      message: "selectReviewCreate query error",
+    });
+  }
+}
+
+// 후기 조회(좋아요순)
+async function selectReviewLike(
+  userIdx,
+  vaccineName1,
+  vaccineName2,
+  vaccineName3
+) {
+  const connection = await pool.getConnection(async (conn) => conn);
+  try {
+    const selectReviewQuery = `
+    select Review.reviewIdx, Review.userIdx,
+       User.userNickname, User.userProfileImgLink, reviewContent, vaccineName,
+       (select count(*) from LikeReview where LikeReview.status = 1 and LikeReview.reviewIdx = Review.reviewIdx) as likeReviewCount,
+       (select CASE WHEN LikeReview.status = 1 THEN 1 ELSE 0 END) as likeStatus,
+       case
+           when TIMESTAMPDIFF(MINUTE,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(SECOND,Review.createdAt,now()), '초전')
+           when TIMESTAMPDIFF(HOUR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MINUTE,Review.createdAt,now()), '분전')
+           when TIMESTAMPDIFF(DAY,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(HOUR,Review.createdAt,now()), '시간전')
+           when TIMESTAMPDIFF(WEEK,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(DAY,Review.createdAt,now()), '일전')
+           when TIMESTAMPDIFF(MONTH,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(WEEK,Review.createdAt,now()), '주전')
+           when TIMESTAMPDIFF(YEAR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MONTH,Review.createdAt,now()), '달전')
+           else concat(TIMESTAMPDIFF(YEAR, Review.createdAt, now()), '년전') end as createdAt
+    from Review
+            left join User on Review.userIdx = User.userIdx
+            left join LikeReview on LikeReview.userIdx = ? and Review.reviewIdx = LikeReview.reviewIdx
+    where if(? is not null,  vaccineName = ?, 1=1) and Review.status = 1
+    union
+    select Review.reviewIdx, Review.userIdx,
+          User.userNickname, User.userProfileImgLink, reviewContent, vaccineName,
+          (select count(*) from LikeReview where LikeReview.status = 1 and LikeReview.reviewIdx = Review.reviewIdx) as likeReviewCount,
+          (select CASE WHEN LikeReview.status = 1 THEN 1 ELSE 0 END) as likeStatus,
+          case
+              when TIMESTAMPDIFF(MINUTE,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(SECOND,Review.createdAt,now()), '초전')
+              when TIMESTAMPDIFF(HOUR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MINUTE,Review.createdAt,now()), '분전')
+              when TIMESTAMPDIFF(DAY,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(HOUR,Review.createdAt,now()), '시간전')
+              when TIMESTAMPDIFF(WEEK,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(DAY,Review.createdAt,now()), '일전')
+              when TIMESTAMPDIFF(MONTH,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(WEEK,Review.createdAt,now()), '주전')
+              when TIMESTAMPDIFF(YEAR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MONTH,Review.createdAt,now()), '달전')
+              else concat(TIMESTAMPDIFF(YEAR, Review.createdAt, now()), '년전') end as createdAt
+    from Review
+            left join User on Review.userIdx = User.userIdx
+            left join LikeReview on LikeReview.userIdx = ? and Review.reviewIdx = LikeReview.reviewIdx
+    where if(? is not null,  vaccineName = ?, 1=1) and Review.status = 1
+    union
+    select Review.reviewIdx, Review.userIdx,
+          User.userNickname, User.userProfileImgLink, reviewContent, vaccineName,
+          (select count(*) from LikeReview where LikeReview.status = 1 and LikeReview.reviewIdx = Review.reviewIdx) as likeReviewCount,
+          (select CASE WHEN LikeReview.status = 1 THEN 1 ELSE 0 END) as likeStatus,
+          case
+              when TIMESTAMPDIFF(MINUTE,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(SECOND,Review.createdAt,now()), '초전')
+              when TIMESTAMPDIFF(HOUR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MINUTE,Review.createdAt,now()), '분전')
+              when TIMESTAMPDIFF(DAY,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(HOUR,Review.createdAt,now()), '시간전')
+              when TIMESTAMPDIFF(WEEK,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(DAY,Review.createdAt,now()), '일전')
+              when TIMESTAMPDIFF(MONTH,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(WEEK,Review.createdAt,now()), '주전')
+              when TIMESTAMPDIFF(YEAR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MONTH,Review.createdAt,now()), '달전')
+              else concat(TIMESTAMPDIFF(YEAR, Review.createdAt, now()), '년전') end as createdAt
+    from Review
+            left join User on Review.userIdx = User.userIdx
+            left join LikeReview on LikeReview.userIdx = ? and Review.reviewIdx = LikeReview.reviewIdx
+    where if(? is not null,  vaccineName = ?, 1=1) and Review.status = 1
+    group by Review.reviewIdx, likeReviewCount
+    order by likeReviewCount desc;
+                  `;
+    const selectReviewParams = [
+      userIdx,
+      vaccineName1,
+      vaccineName1,
+      userIdx,
+      vaccineName2,
+      vaccineName2,
+      userIdx,
+      vaccineName3,
+      vaccineName3,
+    ];
+    const [selectReviewRows] = await connection.query(
+      selectReviewQuery,
+      selectReviewParams
+    );
+    connection.release();
+    return selectReviewRows;
+  } catch (err) {
+    connection.release();
+    return res.json({
+      isSuccess: false,
+      code: 4000,
+      message: "selectReviewLike query error",
+    });
+  }
+}
+
 module.exports = {
   userCheck,
   insertReview,
+  selectReviewCreate,
+  selectReviewLike,
 };
