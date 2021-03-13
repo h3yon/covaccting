d788cf02644ca66776b6e61d1e3b16a0cebde311
강희윤 <khykhy1006@gmail.com>
2021-03-13 11:34:18 +0900

    pull

--- src/app/dao/reviewDao.js
+++ src/app/dao/reviewDao.js
@@ -59,58 +59,103 @@ async function selectReviewCreate(
   const connection = await pool.getConnection(async (conn) => conn);
   try {
     const selectReviewQuery = `
-    select Review.reviewIdx, Review.userIdx,
-       User.userNickname, User.userProfileImgLink, reviewContent, vaccineName,
-       (select count(*) from LikeReview where LikeReview.status = 1 and LikeReview.reviewIdx = Review.reviewIdx) as likeReviewCount,
-       (select CASE WHEN LikeReview.status = 1 THEN 1 ELSE 0 END) as likeStatus,
-       case
-           when TIMESTAMPDIFF(MINUTE,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(SECOND,Review.createdAt,now()), '초전')
-           when TIMESTAMPDIFF(HOUR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MINUTE,Review.createdAt,now()), '분전')
-           when TIMESTAMPDIFF(DAY,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(HOUR,Review.createdAt,now()), '시간전')
-           when TIMESTAMPDIFF(WEEK,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(DAY,Review.createdAt,now()), '일전')
-           when TIMESTAMPDIFF(MONTH,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(WEEK,Review.createdAt,now()), '주전')
-           when TIMESTAMPDIFF(YEAR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MONTH,Review.createdAt,now()), '달전')
-           else concat(TIMESTAMPDIFF(YEAR, Review.createdAt, now()), '년전') end as createdAt
+    select Result.reviewIdx, Result.userIdx, userNickname, userProfileImgLink, Result.vaccineName, likeReviewCount, likeStatus, Result.createdAt
+from (select Review.reviewIdx as reviewIdx,
+        Review.userIdx as userIdx,
+        User.userNickname,
+        User.userProfileImgLink,
+        reviewContent,
+        vaccineName,
+        (select count(*)
+         from LikeReview
+         where LikeReview.status = 1 and LikeReview.reviewIdx = Review.reviewIdx) as likeReviewCount,
+        (select CASE WHEN LikeReview.status = 1 THEN 1 ELSE 0 END)                as likeStatus,
+        case
+            when TIMESTAMPDIFF(MINUTE, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(SECOND, Review.createdAt, now()), '초전')
+            when TIMESTAMPDIFF(HOUR, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(MINUTE, Review.createdAt, now()), '분전')
+            when TIMESTAMPDIFF(DAY, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(HOUR, Review.createdAt, now()), '시간전')
+            when TIMESTAMPDIFF(WEEK, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(DAY, Review.createdAt, now()), '일전')
+            when TIMESTAMPDIFF(MONTH, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(WEEK, Review.createdAt, now()), '주전')
+            when TIMESTAMPDIFF(YEAR, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(MONTH, Review.createdAt, now()), '달전')
+            else concat(TIMESTAMPDIFF(YEAR, Review.createdAt, now()), '년전') end   as createdAt
+ from Review
+          left join User on Review.userIdx = User.userIdx
+          left join LikeReview on LikeReview.userIdx = ? and Review.reviewIdx = LikeReview.reviewIdx
+ where if(? is not null, vaccineName = ?, 1 = 1)
+   and Review.status = 1
+   and User.status = 1
+ union
+ select Review.reviewIdx as reviewIdx,
+        Review.userIdx as userIdx,
+        User.userNickname,
+        User.userProfileImgLink,
+        reviewContent,
+        vaccineName,
+        (select count(*)
+         from LikeReview
+         where LikeReview.status = 1 and LikeReview.reviewIdx = Review.reviewIdx) as likeReviewCount,
+        (select CASE WHEN LikeReview.status = 1 THEN 1 ELSE 0 END)                as likeStatus,
+        case
+            when TIMESTAMPDIFF(MINUTE, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(SECOND, Review.createdAt, now()), '초전')
+            when TIMESTAMPDIFF(HOUR, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(MINUTE, Review.createdAt, now()), '분전')
+            when TIMESTAMPDIFF(DAY, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(HOUR, Review.createdAt, now()), '시간전')
+            when TIMESTAMPDIFF(WEEK, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(DAY, Review.createdAt, now()), '일전')
+            when TIMESTAMPDIFF(MONTH, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(WEEK, Review.createdAt, now()), '주전')
+            when TIMESTAMPDIFF(YEAR, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(MONTH, Review.createdAt, now()), '달전')
+            else concat(TIMESTAMPDIFF(YEAR, Review.createdAt, now()), '년전') end   as createdAt
+ from Review
+          left join User on Review.userIdx = User.userIdx
+          left join LikeReview on LikeReview.userIdx = ? and Review.reviewIdx = LikeReview.reviewIdx
+ where if(? is not null, vaccineName = ?, 1 = 1)
+   and Review.status = 1
+   and User.status = 1
+ union
+ select Review.reviewIdx as reviewIdx,
+        Review.userIdx as userIdx,
+        User.userNickname,
+        User.userProfileImgLink,
+        reviewContent,
+        vaccineName,
+        (select count(*)
+         from LikeReview
+         where LikeReview.status = 1 and LikeReview.reviewIdx = Review.reviewIdx) as likeReviewCount,
+        (select CASE WHEN LikeReview.status = 1 THEN 1 ELSE 0 END)                as likeStatus,
+        case
+            when TIMESTAMPDIFF(MINUTE, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(SECOND, Review.createdAt, now()), '초전')
+            when TIMESTAMPDIFF(HOUR, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(MINUTE, Review.createdAt, now()), '분전')
+            when TIMESTAMPDIFF(DAY, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(HOUR, Review.createdAt, now()), '시간전')
+            when TIMESTAMPDIFF(WEEK, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(DAY, Review.createdAt, now()), '일전')
+            when TIMESTAMPDIFF(MONTH, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(WEEK, Review.createdAt, now()), '주전')
+            when TIMESTAMPDIFF(YEAR, Review.createdAt, now()) < 1
+                then concat(TIMESTAMPDIFF(MONTH, Review.createdAt, now()), '달전')
+            else concat(TIMESTAMPDIFF(YEAR, Review.createdAt, now()), '년전') end   as createdAt
     from Review
-            left join User on Review.userIdx = User.userIdx
-            left join LikeReview on LikeReview.userIdx = ? and Review.reviewIdx = LikeReview.reviewIdx
-    where if(? is not null,  vaccineName = ?, 1=1) and Review.status = 1 and User.status = 1
-    union
-    select Review.reviewIdx, Review.userIdx,
-          User.userNickname, User.userProfileImgLink, reviewContent, vaccineName,
-          (select count(*) from LikeReview where LikeReview.status = 1 and LikeReview.reviewIdx = Review.reviewIdx) as likeReviewCount,
-          (select CASE WHEN LikeReview.status = 1 THEN 1 ELSE 0 END) as likeStatus,
-          case
-              when TIMESTAMPDIFF(MINUTE,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(SECOND,Review.createdAt,now()), '초전')
-              when TIMESTAMPDIFF(HOUR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MINUTE,Review.createdAt,now()), '분전')
-              when TIMESTAMPDIFF(DAY,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(HOUR,Review.createdAt,now()), '시간전')
-              when TIMESTAMPDIFF(WEEK,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(DAY,Review.createdAt,now()), '일전')
-              when TIMESTAMPDIFF(MONTH,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(WEEK,Review.createdAt,now()), '주전')
-              when TIMESTAMPDIFF(YEAR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MONTH,Review.createdAt,now()), '달전')
-              else concat(TIMESTAMPDIFF(YEAR, Review.createdAt, now()), '년전') end as createdAt
-    from Review
-            left join User on Review.userIdx = User.userIdx
-            left join LikeReview on LikeReview.userIdx = ? and Review.reviewIdx = LikeReview.reviewIdx
-    where if(? is not null,  vaccineName = ?, 1=1) and Review.status = 1 and User.status = 1
-    union
-    select Review.reviewIdx, Review.userIdx,
-          User.userNickname, User.userProfileImgLink, reviewContent, vaccineName,
-          (select count(*) from LikeReview where LikeReview.status = 1 and LikeReview.reviewIdx = Review.reviewIdx) as likeReviewCount,
-          (select CASE WHEN LikeReview.status = 1 THEN 1 ELSE 0 END) as likeStatus,
-          case
-              when TIMESTAMPDIFF(MINUTE,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(SECOND,Review.createdAt,now()), '초전')
-              when TIMESTAMPDIFF(HOUR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MINUTE,Review.createdAt,now()), '분전')
-              when TIMESTAMPDIFF(DAY,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(HOUR,Review.createdAt,now()), '시간전')
-              when TIMESTAMPDIFF(WEEK,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(DAY,Review.createdAt,now()), '일전')
-              when TIMESTAMPDIFF(MONTH,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(WEEK,Review.createdAt,now()), '주전')
-              when TIMESTAMPDIFF(YEAR,Review.createdAt,now()) < 1 then concat(TIMESTAMPDIFF(MONTH,Review.createdAt,now()), '달전')
-              else concat(TIMESTAMPDIFF(YEAR, Review.createdAt, now()), '년전') end as createdAt
-    from Review
-            left join User on Review.userIdx = User.userIdx
-            left join LikeReview on LikeReview.userIdx = ? and Review.reviewIdx = LikeReview.reviewIdx
-    where if(? is not null,  vaccineName = ?, 1=1) and Review.status = 1 and User.status = 1
+              left join User on Review.userIdx = User.userIdx
+              left join LikeReview on LikeReview.userIdx = ? and Review.reviewIdx = LikeReview.reviewIdx
+    where if(? is not null, vaccineName = ?, 1 = 1)
+      and Review.status = 1
+      and User.status = 1
+    ) Result
+    left join Review on Result.reviewIdx = Review.reviewIdx
     group by Review.reviewIdx, Review.createdAt
-    order by createdAt desc;
+    order by Review.createdAt desc;
                   `;
     const selectReviewParams = [
       userIdx,
